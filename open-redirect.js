30460dfcc83d11e27bd35f0f26feb898e7fcaabe
fauzi-isobar <65534134+fauzi-isobar@users.noreply.github.com>
2021-06-30 17:16:10 +0800

    Update open-redirect.js

--- open-redirect.js
+++ open-redirect.js
@@ -1,2 +1,14 @@
-var strDest = form.dest.value;
-window.open(strDest,"myresults");
+var features = "status=no, toolbar=no, menubar=no, location=no, top=20, left=175, height=500, width=750";
+
+var baseURL = "https://www.abcd.com.sg";
+
+var strURL = baseURL + "?machineName=" + strMachine + "&template=snoop" +"&context=DEBUG&date=" + strDate.replace(regExp, "");
+
+var validateStrURL =  new RegExp("/^[.\\p{Alnum}\\p{Space}]{0,1024}+/", "g");
+if(validateStrURL.test(strURL))
+{
+  if((strURL != null)&&(strURL.length !=0)&&(strURL.length <= 200) )
+  {
+    window.open(strURL,"_blank",features);
+  }
+}
